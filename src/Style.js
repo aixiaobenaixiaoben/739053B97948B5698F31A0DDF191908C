fab49b725fd5634585de2a360a96576da8eceebf
kevin <357620917@qq.com>
2018-09-12 10:34:44 +0800

    update Style

--- src/Style.js
+++ src/Style.js
@@ -3,13 +3,13 @@
 /** color */
 export const COLOR_SYS = '#D6413B'
 export const COLOR_BLACK_SYS = '#3A3A3A'
-export const COLOR_WHITE = 'white'
-export const COLOR_RED = 'red'
-export const COLOR_RED_02 = 'rgba(252, 13, 27, 0.2)'
-export const COLOR_RED_04 = 'rgba(252, 13, 27, 0.4)'
-export const COLOR_GREEN = 'green'
-export const COLOR_GRAY = 'gray'
-export const COLOR_GRAY_LIGHT = 'lightgray'
+export const COLOR_WHITE = '#FFFFFF'
+export const COLOR_RED = '#FF6666'
+export const COLOR_RED_02 = 'rgba(255, 102, 102, 0.2)'
+export const COLOR_RED_04 = 'rgba(255, 102, 102, 0.4)'
+export const COLOR_GREEN = '#008000'
+export const COLOR_GRAY = '#808080'
+export const COLOR_GRAY_LIGHT = '#D3D3D3'
 
 /** font */
 export const FONT_SIZE_12 = 12
