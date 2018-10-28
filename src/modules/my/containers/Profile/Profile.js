e64085722f20bd0bc0e6bde232af8d7f25ddc8a4
kevin <357620917@qq.com>
2018-10-28 20:27:26 +0800

    update user info

--- src/modules/my/containers/Profile/Profile.js
+++ src/modules/my/containers/Profile/Profile.js
@@ -4,37 +4,56 @@ import {Image, ScrollView} from "react-native"
 import {List, WhiteSpace} from "antd-mobile-rn"
 import {connect} from "react-redux"
 import PropTypes from "prop-types"
+import style from "../styles/Profile/Profile"
+import {ACTION_PROFILE_PATH_UPDATE} from "../../Constants"
+import * as ftpActions from "../../../common/actions/FTP"
 
 const Item = List.Item
 
 class Profile extends Component<any, any> {
 
+  modifyPhoto = () => {
+    alert(1)
+  }
+
+  modifyName = () => {
+    alert(1)
+  }
+
+  modifyGender = () => {
+    alert(1)
+  }
+
+  getPhoto = () => {
+    const {profile: {spfphotog = ''}, photoPath} = this.props
+    if (spfphotog != null && spfphotog.length > 0 && photoPath.indexOf(spfphotog) === -1) {
+      this.props.download(ACTION_PROFILE_PATH_UPDATE, spfphotog)
+    }
+    if (photoPath.length > 0) {
+      return <Image style={style.image} source={{uri: photoPath}}/>
+    }
+    return <Image style={style.image} source={require('../../../../../assets/my/profile/logo01.png')}/>
+  }
+
   render() {
     let {user: {suiusrnam}} = this.props
 
     return (
-      <ScrollView>
+      <ScrollView style={style.scroll}>
         <WhiteSpace size="lg"/>
         <List>
-          <Item arrow="horizontal" onClick={() => alert(1)}
-                extra={<Image source={require('../../../../../assets/touchid.png')} style={{width: 60, height: 60}}/>}>
+          <Item style={style.listItem} arrow="horizontal" onClick={this.modifyPhoto} extra={this.getPhoto()}>
             头像
           </Item>
         </List>
 
         <WhiteSpace size="lg"/>
         <List>
-          <Item arrow="horizontal" onClick={() => alert(1)} extra={suiusrnam}>
-            用户名
+          <Item style={style.listItem} arrow="horizontal" onClick={this.modifyName} extra={suiusrnam}>
+            昵称
           </Item>
-          <Item arrow="horizontal" onClick={() => alert(1)} extra={suiusrnam}>
+          <Item style={style.listItem} arrow="horizontal" onClick={this.modifyGender} extra={suiusrnam}>
             性别
-          </Item>
-          <Item arrow="horizontal" onClick={() => alert(1)} extra={suiusrnam}>
-            地区
-          </Item>
-          <Item arrow="horizontal" onClick={() => alert(1)} extra={suiusrnam}>
-            签名
           </Item>
         </List>
       </ScrollView>
@@ -44,10 +63,18 @@ class Profile extends Component<any, any> {
 
 Profile.propTypes = {
   user: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+  photoPath: PropTypes.string.isRequired,
+  download: PropTypes.func.isRequired,
 }
 
 export default connect(
   state => ({
     user: state.common.login.user,
+    profile: state.my.profile.profile,
+    photoPath: state.my.profile.photoPath,
+  }),
+  dispatch => ({
+    download: (action: string, fileName: string) => dispatch(ftpActions.download(action, fileName)),
   })
 )(Profile)
---
+++ src/modules/my/containers/styles/Profile/Profile.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from "react-native"
+import {COLOR_WHITE_MORE_DARK} from "../../../../../Style"
+
+export default StyleSheet.create({
+  scroll: {
+    backgroundColor: COLOR_WHITE_MORE_DARK,
+  },
+  listItem: {
+    paddingLeft: 20,
+    paddingRight: 5,
+  },
+  image: {
+    width: 64,
+    height: 64,
+    resizeMode: 'contain',
+    borderRadius: 32,
+  },
+})
